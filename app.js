944d106bae089d13c4637107678509291f53a024
Theseregrets <rishitashaw49@gmail.com>
2021-05-06 19:31:33 +0530

    first draft

---
+++ app.js
@@ -0,0 +1,61 @@
+const button = document.getElementById('button')
+const content = document.querySelector('.content')
+
+//arrays of replies
+
+const greetings = [
+    'im good you little piece of sugar crush',
+    'i will be good. care about yourself',
+    'leave me alone'
+]
+
+const weather = [
+    'why do you even care all you do is stay at home and eat',
+    'weather is great but you will stay at home so why do you care'
+]
+const work =
+    'you should work now and build something real intead of doing meaningless work and being a burden on earth. you little piece of shit'
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function () {
+    console.log('voice activated')
+}
+
+recognition.onresult = function (event) {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript
+
+    content.textContent = transcript;
+    readOutLoud(transcript)
+}
+
+button.addEventListener('click', () => {
+    recognition.start();
+})
+
+function readOutLoud(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = 'i dont know what you are saying'
+    if (message.includes('how are you')) {
+        const finalText = greetings[Math.floor(Math.random() * greetings.length)]
+        speech.text = finalText;
+    }
+    else if (message.includes('weather')) {
+        const finalText = weather[Math.floor(Math.random() * greetings.length)]
+        speech.text = finalText;
+    }
+    else if (message.includes('are you working')) {
+        const finalText = work
+        speech.text = finalText;
+    }
+
+
+    speech.volume = 1;
+    speech.rate = 0.8;
+    window.speechSynthesis.speak(speech)
+}
+
+//
